4fa51071f0d1b43a26d7852973f39a3c0046540d
teachertophoon <teachertophoon@gmail.com>
2021-09-10 11:59:41 +0900

    Post 모델을 생성하고 시간 설정하기

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Post # 우리가 만든 Post 클래스 불러오기
 
-# Register your models here.
+admin.site.register(Post) # Post 클래스를 관리자 사이트에 등록
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author: 추후 작성 예정
+
+    # 관리자 페이지에서 작성한 Post의 제목 구성을 바꾸고 싶을 때
+    def __str__(self):
+        return f'[{self.pk}] {self.title}' # f는 format을 의미
