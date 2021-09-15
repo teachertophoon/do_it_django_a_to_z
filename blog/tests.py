b1783dc6a5428b83903a389afc6e274d47b56c0e
teachertophoon <teachertophoon@gmail.com>
2021-09-15 14:35:05 +0900

    포스트 목록에 포스트가 하나도 없는 경우와 포스트 상세 페이지 테스트하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from bs4 import BeautifulSoup
+from django.test import TestCase, Client
 
-# Create your tests here.
+from blog.models import Post
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    # 테스트할 함수명 앞에는 꼭 test_를 붙여 주셔야 합니다.
+    def test_post_list(self):
+        # 두 개의 값을 비교해서 같으면 테스트 통과
+        # 첫 번째 파라메터는 실제 결과 값
+        # 두 번째 파라메터는 비교 대상
+        # self.assertEqual(2, 2)
+
+        # 1.1 포스트 목록 페이지를 가져온다.
+        response = self.client.get('/blog/')
+        # 1.2 정상적으로 페이지가 로드된다.
+        self.assertEqual(response.status_code, 200)
+        # 1.3 페이지 타이틀은 'Blog'이다.
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Blog')
+        # 1.4 내비게이션 바가 있다.
+        navbar = soup.nav
+        # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        # 2.1 메인 영역에 게시물이 하나도 없다면
+        self.assertEqual(Post.objects.count(), 0)
+        # 2.2 '아직 게시물이 없습니다' 라는 문구가 보인다.
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다', main_area.text)
+
+        # 3.1 게시물이 2개 있다면
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+        )
+        post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='1등이 전부는 아니잖아요?',
+        )
+        self.assertEqual(Post.objects.count(), 2)
+
+        # 3.2 포스트 목록 페이지를 새로고침 했을 때
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+
+        # 3.3 메인 영역에 포스트 2개의 타이틀이 존재한다.
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+
+        # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+    # 포스트 상세페이지 테스트
+    def test_post_detail(self):
+        # 1.1 포스트가 하나 있다.
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+        )
+        # 1.2 그 포스트의 url은 '/blog/1/' 이다.
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # 2. 첫 번째 포스트의 상세 페이지 테스트
+        # 2.1 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다(status code: 200)
+        # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
+        # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
+        # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
+        # 2.5 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다.(아직 구현할 수 없음)
+        # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
