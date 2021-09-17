654ec68359364a4affa5f99c881ee613f6810dcf
teachertophoon <teachertophoon@gmail.com>
2021-09-17 12:12:36 +0900

    카테고리 테이블 추가하기(317쪽)

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Post # 우리가 만든 Post 클래스 불러오기
+from .models import Post, Category  # 우리가 만든 Post 클래스 불러오기
 
 admin.site.register(Post) # Post 클래스를 관리자 사이트에 등록
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,16 @@ import os
 from django.contrib.auth.models import User
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -15,6 +25,7 @@ class Post(models.Model):
     # CASCADE: 작성자 정보가 User 테이블에서 삭제되면 작성했던 글도 함께 삭제
     # SET_NULL: 작성자 정보가 User 테이블에서 삭제되면 작성했던 글은 유지(author는 null)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     # 관리자 페이지에서 작성한 Post의 제목 구성을 바꾸고 싶을 때
     def __str__(self):
@@ -28,3 +39,5 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
