f506899c39dae7f5358d3f6df85bb2718de0862e
teachertophoon <teachertophoon@gmail.com>
2021-11-15 09:52:01 +0900

    11월 15일 1교시

--- api/views.py
+++ api/views.py
@@ -1,15 +1,13 @@
 import json
 
+from django.core import serializers
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
-
-# CBV 방식
 from django.views.decorators.csrf import csrf_exempt
-
 from api.models import Car, DHT
 
-
+# CBV 방식
 class TestView(View):
 
     # GET 요청을 처리하기 위해서 get 함수를 재정의
@@ -54,6 +52,19 @@ class DHTView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(DHTView, self).dispatch(request, *args, **kwargs)
+
+    # GET 요청을 처리하기 위해서 get 함수를 재정의
+    def get(self, request, *args, **kwargs):
+
+        # DHT.objects.all()의 결과는 QuerySet 타입이다.
+        # 아래 코드는 QuerySet 타입을 json 타입으로 변경하고
+        # json 객체를 직렬화 한 것이다.
+        dhts = serializers.serialize("json", DHT.objects.all())
+        print(dhts)
+        return JsonResponse(dhts, # 직렬화한 json 객체
+                            safe=False, # 첫번째 파라미터의 타입이 딕셔너리가 아닐경우
+                            json_dumps_params={'ensure_ascii': False}, # 한글지원
+                            status=200)
 
     # POST 요청을 처리하기 위해서 post 함수를 재정의
     def post(self, request, *args, **kwargs):
